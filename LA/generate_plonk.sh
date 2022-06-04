712e29e26b7e819500757c91b781aa8db3b0d41e
spagnolog <gasper.spagnolo@outlook.com>
2022-06-04 17:05:57 +0200

    popravki

--- LA/generate_plonk.sh
+++ LA/generate_plonk.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-sed '6 i \\\usepackage[fontsize=5pt]{fontsize}' la.tex > ./plonk/la-plonk.tex && printf "Success!\n" && exit
-printf "Err!\n"
+
+function throw_err {
+    printf "Err!\n"
+    exit 1
+}
+
+sed '6 i \\\usepackage[fontsize=5pt]{fontsize}' la.tex > ./plonk/la-plonk.tex || throw_err
+sed -i '14s/3/4/' ./plonk/la-plonk.tex || throw_err
+
+printf "Success!\n"
